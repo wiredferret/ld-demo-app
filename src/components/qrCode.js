30d9a257fc92a68faffe469833eb3ddf2d3ab1a8
Cody De Arkland <codydearkland@gmail.com>
2022-03-17 14:19:47 -0700

    fixing qr code

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code";
 // import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 //Change QRURL to the URL where you'll be hosting this app
-const QRURL = "https://codyde.github.io/ld-demo-himms/";
+const QRURL = "https://codyde.github.io/ld-demo-app/";
 
 const qrCodeHome = () => {  
   return (
