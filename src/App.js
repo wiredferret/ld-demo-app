aed5546e13a2a40ce4227ce534c2b4649efcdf00
Cody De Arkland <codydearkland@gmail.com>
2022-03-17 22:07:24 -0700

    adding new cards

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import "./App.css";
 import { useFlags } from "launchdarkly-react-client-sdk";
 import Header from "./components/header";
 import Login from "./components/login";
+import Cards from "./components/cards";
 
 function App() {
   const { qrcode, upperimage, logoversion } = useFlags();
 
   return (
     <div className="App h-screen bg-ldls grid xl:grid-cols-4 grid-rows-3 bg-cover bg-no-repeat">
-    <div className="body grid row-start-3 xl:col-start-1 xl:row-start-2 bg-black-4 place-items-center shadow-xl">
+    <div className="body grid row-start-3 xl:col-start-1 xl:row-start-2 bg-black-4 place-items-center">
     {qrcode ? <QRCode /> : <Login />}
     </div>
     {upperimage ? 
     <div className="body grid row-start-2 xl:row-start-1 xl:col-start-3 place-items-center">
       {logoversion ? (
-        <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />
+        <img src={toggle} className="mx-auto h-56 lg:h-60 2xl:h-80" alt="logo" />
       ) : (
         <div className="">
-          <img src={ldlogo} className="App-pulse mx-auto h-56 lg:h-80" alt="logo" />
+          <img src={ldlogo} className="App-pulse mx-auto h-56 lg:h-60 2xl:h-80" alt="logo" />
         </div>
       )}
     </div>
@@ -28,6 +29,9 @@ function App() {
     <header className="App-header grid row-start-1 xl:row-start-2 xl:col-start-2 xl:col-span-3">
       <Header />
     </header>
+    <div className="body grid xl:row-start-3 xl:col-start-2 xl:col-span-3 invisible xl:visible">
+    <Cards />
+    </div>
   </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,12 @@ import ld from '../ld-white-wide.png'
 export default function Header() {
   return (
     <div className='grid  w-full bg-ldgray shadow-2xl place-items-center'>
-    <div className="mx-auto w-full p-4 lg:p-4 px-7 lg:px-28">
+    <div className="mx-auto xl:w-full p-4 lg:p-4 px-7 lg:px-28">
       <div>
       <img src={ld} className="mx-auto max-h-30" alt="logo" />
       </div>
-      <div className="text-base md:text-3xl pb-4 xl:text-6xl">
-        <h1>Ship and release <span className="text-transparent bg-clip-text bg-gradient-to-br from-ldyellow to-lddblue">features</span> faster!</h1>
+      <div className="text-base md:text-4xl pb-4 2xl:text-6xl">
+        <p>Ship and release <span className="text-transparent bg-clip-text bg-gradient-to-br from-ldyellow to-lddblue">features</span> faster!</p>
       </div>      
     </div>
     </div>
