4aec71d88430b41297a19d4979d47242d83d4913
Cody De Arkland <codydearkland@gmail.com>
2022-03-21 09:59:42 -0700

    Resetting

--- src/App.js
+++ src/App.js
@@ -13,20 +13,20 @@ function App() {
 
   return (
     <div className="App h-screen bg-ldls grid xl:grid-cols-4 grid-rows-3 bg-cover bg-no-repeat">
-      { prodHeader ? (
-      <header className="App-header grid col-start-1 col-span-4 row-start-2">
+      {/* { prodHeader ? (
+        <header className="App-header grid row-start-1 xl:row-start-2 xl:col-start-2 xl:col-span-3">
+        <Header />
+      </header>
+      ):
+      ( */}
+        <header className="App-header grid col-start-1 col-span-4 row-start-2">
         <div className="body bg-ldgray px-8 py-4 mb-5 w-full shadow-2xl">
           <p className="text-sm xl:text-4xl">The common <span className="text-ldyellow">Application Delivery</span> flow...</p>
         </div>
         <Intro />
       </header>
-      ):
-      (
-        <header className="App-header grid row-start-1 xl:row-start-2 xl:col-start-2 xl:col-span-3">
-          <Header />
-        </header>
-        )
-      }
+        {/* )
+      } */}
       <div className="body grid row-start-3 xl:col-start-1 xl:row-start-2 bg-black-4 place-items-center">
         {qrcode ? (
           <div>
