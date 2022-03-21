d6935f66b379341d5d5c5496bec14c4574520214
Cody De Arkland <codydearkland@gmail.com>
2022-03-21 11:59:45 -0700

    Reset

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             <p className="text-sm xl:text-4xl">The common <span className="text-ldyellow">Application Delivery</span> flow...</p>
           </div>
         <Intro />
+
         </header>
         {/* )
       } */}
