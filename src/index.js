0f4c2fb0fd4718402688454c6ecfcac8a466e93d
Cody De Arkland <codydearkland@gmail.com>
2022-07-06 15:33:05 -0700

    Removing SDK key and making a variable

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ let id = getUserId();
 
 (async () => {
   const LDProvider = await asyncWithLDProvider({
-    clientSideID: '6240170984a211152a6539b3',
+    clientSideID: process.env.REACT_APP_LD_SDK_KEY,
     user: {
       key: id,
       custom: {
